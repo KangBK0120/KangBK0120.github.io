a19348dfd9393e24de58f03502b17554188286ff
KangBK0120 <lejkbk0930@gmail.com>
2025-05-06 15:47:16 +0900

    feat: add dependecy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), icon(), expressiveCode(
+    {
+      themes: ['dracula'],
+      styleOverrides: {
+        codeFontFamily: 'IBM Plex Mono, monospace',
+        codeFontSize: '0.875rem',
+      }
+    }
+  )],
+  markdown: {
+    remarkPlugins: ['remark-gfm', 'remark-smartypants', 'remark-math'],
+    rehypePlugins: ['rehype-katex'],
+  }
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+			  sans: ['Nanum Gothic', 'sans-serif'],
+			  serif: ['Pretendard', 'serif'],
+			  code: ['IBM Plex Mono', 'monospace'],
+			},
+		  },
+	},
+	plugins: [],
+}
